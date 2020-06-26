4b9505f7bc0710dcc2c8004e3d0ace7662a85400
Irati Larreina <irati_6@hotmail.com>
2020-06-26 10:48:24 +0200

    added homepage

--- CodeReview07/src/app/app.module.ts
+++ CodeReview07/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HeroComponent } from './hero/hero.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HeroComponent
+    HeroComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
